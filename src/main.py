7fedee103eaf26751b0ec194a0462d0fbfdef8a7
Francesca Behrens <francesca.behrens@mail.polimi.it>
2025-10-27 18:02:07 +0100

    implementing prepocessing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from prepocessing import Dataset
 import pandas as pd
 
-dataset = Dataset('dataset/train.csv')
+dataset = Dataset('../dataset/train.csv')
 
 dataset.load_data()
 dataset.visualizzazione() 
 
+dataset.drop_columns(["Id", "Utilities"])
+
+dataset.data.info()
+
+dataset.convert_ordinal("ExterQual", ['Po', 'Fa', 'TA', 'Gd', 'Ex'])
+
+print(dataset.data["ExterQual"])
--- src/prepocessing.py
+++ src/prepocessing.py
@@ -23,3 +23,29 @@ class Dataset:
             self.data = self.data.drop(columns=columns)
         else:
             print("Data not loaded. Please load the data first.")
+
+    def fill_NaN (self):
+        for cols in self.data.columns:
+            if self.data[cols].isnull().any():
+                if self.data[cols].dtype() == 'int64':
+                    self.fill_NaN_int(cols)
+                elif self.data[cols].dtype() == 'float64':
+                    self.fill_NaN_double(cols)
+                else:
+                    self.fill_NaN_string(cols)
+
+    def fill_NaN_int (self, col):
+        median = self.data[col].median()
+        self.data[col].fillna(median)
+
+    def fill_NaN_double (self, col):
+        mean = self.data[col].mean()
+        self.data[col].fillna(mean)
+
+    def fill_NaN_string (self, col):
+        mode = self.data[col].mode()
+        self.data[col].fillna(mode)
+
+    def convert_ordinal (self, col, values):
+        for i, v in enumerate(values):
+            self.data[col].loc[self.data[col] == v] = i
