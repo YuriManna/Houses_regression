6e81c181a5d3ec176d1a97acdcd32149cb3d4e34
Francesca Behrens <francesca.behrens@mail.polimi.it>
2025-10-28 16:50:57 +0100

    change Modello class

--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+from abc import abstractclassmethod
+
 from sklearn.linear_model import LinearRegression , Ridge as SKRidge
 from sklearn.model_selection import train_test_split
 import pandas as pd
@@ -5,15 +7,15 @@ from prepocessing import Dataset
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-class LinReg:
+class Modello:
     def __init__(self, dataset):
         self.dataset = dataset
-        self.model = LinearRegression()
+        self.model = None
         self.X_train = None
         self.y_train = None
         self.X_test = None
         self.y_test = None
-    
+
     def split_data(self, test_size=0.2):
         y = self.dataset.data["SalePrice"]
         X = self.dataset.data.drop("SalePrice", axis=1)
@@ -24,14 +26,14 @@ class LinReg:
 
     def prediction(self):
         return self.model.predict(self.X_test)
-    
+
     def evaluate(self):
         score = self.model.score(self.X_test, self.y_test)
         y_pred = self.model.predict(self.X_test)
         self.plots(y_pred)
-        
+
         return score
-    
+
     def plots(self, y_pred):
         sns.set_theme(style="whitegrid")
 
@@ -50,53 +52,12 @@ class LinReg:
         plt.title("Residual Plot")
         plt.show()
 
+class LinReg(Modello):
+    def __init__(self, dataset):
+        super().__init__(dataset)
+        self.model = LinearRegression()
 
 class Ridge:
     def __init__(self, dataset):
-        self.dataset = dataset
+        super().__init__(dataset)
         self.model = SKRidge(alpha=1.0, random_state=42)
-        self.X_train = None
-        self.y_train = None
-        self.X_test = None
-        self.y_test = None
-    
-    def split_data(self, test_size=0.2):
-        y = self.dataset.data["SalePrice"]
-        X = self.dataset.data.drop("SalePrice", axis=1)
-        self.X_train, self.X_test,self.y_train, self.y_test = train_test_split(X, y, test_size=test_size, random_state=42)
-
-    def train(self):
-        self.model.fit(self.X_train, self.y_train)
-
-    def prediction(self):
-        return self.model.predict(self.X_test)
-    
-    def evaluate(self):
-        score = self.model.score(self.X_test, self.y_test)
-        y_pred = self.model.predict(self.X_test)
-        self.plots(y_pred)
-        
-        return score
-    
-    def plots(self, y_pred):
-        sns.set_theme(style="whitegrid")
-        
-        plt.scatter(self.y_test, y_pred, alpha=0.6)
-        plt.xlabel("Actual")
-        plt.ylabel("Predicted")
-        plt.title("Predicted vs Actual")
-        plt.plot([self.y_test.min(), self.y_test.max()], [self.y_test.min(), self.y_test.max()], 'r--')
-        plt.show()
-
-        residuals = self.y_test - y_pred
-        plt.scatter(y_pred, residuals, alpha=0.6)
-        plt.axhline(0, color='r', linestyle='--')
-        plt.xlabel("Predicted values")
-        plt.ylabel("Residuals")
-        plt.title("Residual Plot")
-        plt.show()
-
-
-
-    
-
