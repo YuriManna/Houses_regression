c5b93871445e83842a810982318195db12380fe3
Yuri Manna <yurimanna@gmail.com>
2025-10-28 15:14:48 +0100

    models and tests

--- src/main.py
+++ src/main.py
@@ -1,63 +1,86 @@
 from prepocessing import Dataset
-from model import Modello
+from model import LinReg, Ridge
 import pandas as pd
 
-dataset = Dataset('../dataset/test.csv')
+def process_dataset(file_path, output_path):
+        
+    # Process training dataset
+    dataset = Dataset(file_path)
 
-dataset.load_data()
-dataset.visualizzazione() 
+    dataset.load_data()
+    # dataset.visualizzazione() 
 
-dataset.drop_columns(["Id", "Utilities"])
+    dataset.drop_columns(["Id", "Utilities"])
 
-dataset.data.info()
+    # dataset.data.info()
 
-regularity_map = {'IR3': 0, 'IR2': 1, 'IR1': 2, 'Reg': 3}
-uti_map = {'ELO': 0, 'NoSeWa': 1, 'NoSewr': 2, 'AllPub': 3}
-slope_map = {'Sev': 0, 'Mod': 1, 'Gtl': 2}
-quality_map = {'NA': 0, 'Po': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5}
-rating_map = {'NA': 0, 'Unf': 1, 'LwQ': 2, 'Rec': 3, 'BLQ': 4, 'ALQ': 5, 'GLQ': 6}
-functionality_map = {'Sal': 0, 'Sev': 1, 'Maj2': 2, 'Maj1': 3, 'Mod': 4, 'Min2': 5, 'Min1': 6, 'Typ': 7}
-finished_map = {'NA': 0, 'Unf': 1, 'RFn': 2, 'Fin': 3}
-fence_map = {'NA': 0, 'MnWw': 1, 'GdWo': 2, 'MnPrv': 3, 'GdPrv': 4}
-exposure_map = {'NA': 0, 'No': 1, 'Mn': 2, 'Av': 3, 'Gd': 4}
+    regularity_map = {'IR3': 0, 'IR2': 1, 'IR1': 2, 'Reg': 3}
+    uti_map = {'ELO': 0, 'NoSeWa': 1, 'NoSewr': 2, 'AllPub': 3}
+    slope_map = {'Sev': 0, 'Mod': 1, 'Gtl': 2}
+    quality_map = {'NA': 0, 'Po': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5}
+    rating_map = {'NA': 0, 'Unf': 1, 'LwQ': 2, 'Rec': 3, 'BLQ': 4, 'ALQ': 5, 'GLQ': 6}
+    functionality_map = {'Sal': 0, 'Sev': 1, 'Maj2': 2, 'Maj1': 3, 'Mod': 4, 'Min2': 5, 'Min1': 6, 'Typ': 7}
+    finished_map = {'NA': 0, 'Unf': 1, 'RFn': 2, 'Fin': 3}
+    fence_map = {'NA': 0, 'MnWw': 1, 'GdWo': 2, 'MnPrv': 3, 'GdPrv': 4}
+    exposure_map = {'NA': 0, 'No': 1, 'Mn': 2, 'Av': 3, 'Gd': 4}
 
-for col, mapping in [("LotShape", regularity_map),
-                     #("Utilities", uti_map),
-                     ("LandSlope", slope_map),
-                     ("ExterQual", quality_map),                     
-                     ("ExterCond", quality_map),                     
-                     ("BsmtQual", quality_map),                     
-                     ("BsmtCond", quality_map),
-                     ("BsmtExposure", exposure_map),
-                     ("BsmtFinType1", finished_map),
-                     ("BsmtFinType2", finished_map),
-                     ("HeatingQC", quality_map),
-                     ("KitchenQual", quality_map),
-                     ("Functional", functionality_map),
-                     ("FireplaceQu", quality_map),
-                     ("GarageFinish", finished_map),
-                     ("GarageQual", quality_map),
-                     ("GarageCond", quality_map),
-                     ("PoolQC", quality_map),
-                     ("Fence", fence_map)]:
-    dataset.map_column(col, mapping)
+    for col, mapping in [("LotShape", regularity_map),
+                        #("Utilities", uti_map),
+                        ("LandSlope", slope_map),
+                        ("ExterQual", quality_map),                     
+                        ("ExterCond", quality_map),                     
+                        ("BsmtQual", quality_map),                     
+                        ("BsmtCond", quality_map),
+                        ("BsmtExposure", exposure_map),
+                        ("BsmtFinType1", finished_map),
+                        ("BsmtFinType2", finished_map),
+                        ("HeatingQC", quality_map),
+                        ("KitchenQual", quality_map),
+                        ("Functional", functionality_map),
+                        ("FireplaceQu", quality_map),
+                        ("GarageFinish", finished_map),
+                        ("GarageQual", quality_map),
+                        ("GarageCond", quality_map),
+                        ("PoolQC", quality_map),
+                        ("Fence", fence_map)]:
+        dataset.map_column(col, mapping)
 
-dataset.convert_nominal(["MSZoning", "Street", "Alley", "LandContour", "LotConfig", "Neighborhood", "Condition1", "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl", "Exterior1st", "Exterior2nd", "MasVnrType", "Foundation", "Heating", "CentralAir", "Electrical", "GarageType", "PavedDrive", "MiscFeature", "SaleType", "SaleCondition"])
+    dataset.convert_nominal(["MSZoning", "Street", "Alley", "LandContour", "LotConfig", "Neighborhood", "Condition1", "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl", "Exterior1st", "Exterior2nd", "MasVnrType", "Foundation", "Heating", "CentralAir", "Electrical", "GarageType", "PavedDrive", "MiscFeature", "SaleType", "SaleCondition"])
 
-dataset.convert_string_to_number("MasVnrArea")
-dataset.convert_string_to_number("LotFrontage")
-dataset.convert_string_to_number("GarageYrBlt")
+    dataset.convert_string_to_number("MasVnrArea")
+    dataset.convert_string_to_number("LotFrontage")
+    dataset.convert_string_to_number("GarageYrBlt")
+
+    dataset.fill_NaN()
+
+    dataset.visualizzazione()
+
+    dataset.export_data(output_path)
+
+    return dataset
+
+dataset = process_dataset("../dataset/train.csv", "../dataset/train_clean.csv")
+# dataset_test = process_dataset("data/test.csv", "data/test_clean.csv")
+# print(dataset.data.corr(numeric_only=True)['SalePrice'].sort_values(ascending=False))
+
+modello_reg = LinReg(dataset)
+modello_reg.split_data()
+modello_reg.train()
+results = modello_reg.prediction()
+score = modello_reg.evaluate()
+print(f"Model R^2 score: {score}")
+#importance = pd.Series(modello_reg.model.coef_, index=modello_reg.X_train.columns)
+#importance.sort_values().plot(kind='barh')
+# print(f"Most important features: {importance.head(10)}")
+#print(f"Intercept: {modello_reg.model.intercept_}")
 
 
+print("\n\nRidge Regression Model")
+modello_ridge = Ridge(dataset)
+modello_ridge.split_data()  
+modello_ridge.train()
+results_ridge = modello_ridge.prediction()
+score_ridge = modello_ridge.evaluate()
+print(f"Ridge Model R^2 score: {score_ridge}")
 
-dataset.fill_NaN()
 
-dataset.export_data('../dataset/test_clean.csv')
-'''
-dataset = Dataset('../dataset/train_clean.csv')
-dataset.load_data()
-model = Modello(dataset)
-model.train()
-
-model.prediction
-'''
--- src/model.py
+++ src/model.py
@@ -1,24 +1,100 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression , SGDRegressor
+from sklearn.model_selection import train_test_split
 import pandas as pd
 from prepocessing import Dataset
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-class Modello:
+class LinReg:
     def __init__(self, dataset):
         self.dataset = dataset
         self.model = LinearRegression()
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
+    
+    def split_data(self, test_size=0.2):
+        y = self.dataset.data["SalePrice"]
+        X = self.dataset.data.drop("SalePrice", axis=1)
+        self.X_train, self.X_test,self.y_train, self.y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 
     def train(self):
-        X = self.dataset.data.drop("SalePrice", axis=1)
-        y = self.dataset.data["SalePrice"]
-        self.model.fit(X, y)
+        self.model.fit(self.X_train, self.y_train)
 
-    def prediction(self, X_new):
-        return self.model.predict(X_new)
+    def prediction(self):
+        return self.model.predict(self.X_test)
     
-    def evaluate(self, X_test, y_test):
-        score = self.model.score(X_test, y_test)
+    def evaluate(self):
+        score = self.model.score(self.X_test, self.y_test)
+        y_pred = self.model.predict(self.X_test)
+        self.plots(y_pred)
+        
         return score
     
+    def plots(self, y_pred):
+        sns.set_theme(style="whitegrid")
+
+        plt.scatter(self.y_test, y_pred, alpha=0.6)
+        plt.xlabel("Actual")
+        plt.ylabel("Predicted")
+        plt.title("Predicted vs Actual")
+        plt.plot([self.y_test.min(), self.y_test.max()], [self.y_test.min(), self.y_test.max()], 'r--')
+        plt.show()
+
+        residuals = self.y_test - y_pred
+        plt.scatter(y_pred, residuals, alpha=0.6)
+        plt.axhline(0, color='r', linestyle='--')
+        plt.xlabel("Predicted values")
+        plt.ylabel("Residuals")
+        plt.title("Residual Plot")
+        plt.show()
+
+
+class Ridge:
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.model = SGDRegressor()
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
+    
+    def split_data(self, test_size=0.2):
+        y = self.dataset.data["SalePrice"]
+        X = self.dataset.data.drop("SalePrice", axis=1)
+        self.X_train, self.X_test,self.y_train, self.y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+
+    def train(self):
+        self.model.fit(self.X_train, self.y_train)
+
+    def prediction(self):
+        return self.model.predict(self.X_test)
+    
+    def evaluate(self):
+        score = self.model.score(self.X_test, self.y_test)
+        y_pred = self.model.predict(self.X_test)
+        self.plots(y_pred)
+        
+        return score
+    
+    def plots(self, y_pred):
+        sns.set_theme(style="whitegrid")
+        
+        plt.scatter(self.y_test, y_pred, alpha=0.6)
+        plt.xlabel("Actual")
+        plt.ylabel("Predicted")
+        plt.title("Predicted vs Actual")
+        plt.plot([self.y_test.min(), self.y_test.max()], [self.y_test.min(), self.y_test.max()], 'r--')
+        plt.show()
+
+        residuals = self.y_test - y_pred
+        plt.scatter(y_pred, residuals, alpha=0.6)
+        plt.axhline(0, color='r', linestyle='--')
+        plt.xlabel("Predicted values")
+        plt.ylabel("Residuals")
+        plt.title("Residual Plot")
+        plt.show()
 
 
 
